380e4316e3061656f73ade89b2c787e3427f3c31
Hilgo <lucas.stabile93@gmail.com>
2026-04-17 18:00:51 -0300

    Ajuste de tamanho máximo do modal

--- Sala do Futuro/lançar_notas_planilha.user.js
+++ Sala do Futuro/lançar_notas_planilha.user.js
@@ -304,7 +304,7 @@ function inicializar(){
     width: 50%;
     border-radius: 5px;
     box-shadow: 0 4px 8px rgba(0,0,0,0.2);
-    max-height: 800px;
+    max-height: 580px;
     overflow-y: auto;
 }
 
